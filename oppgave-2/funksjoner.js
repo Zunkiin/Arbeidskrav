cba47ce63a33a3217c1b74adf7764821f4517198
Zunkiin <zunkinx@gmail.com>
2025-10-16 18:22:20 +0200

    fs

--- oppgave-2/funksjoner.js
+++ oppgave-2/funksjoner.js
@@ -1,8 +1,10 @@
-/* funksjoner.js
-   JavaScript-funksjoner brukt i applikasjon
-*/
-
-// Viser en advarsel før sletting av student eller klasse
-function bekreftSletting() {
-  return confirm("Er du sikker på at du vil slette dette?");
+/* funksjoner.js - felles JS-funksjoner */
+function bekreftSletting(type) {
+  // type er valgfritt (f.eks. 'student' eller 'klasse')
+  if (!type) type = "posten";
+  return confirm("Er du sikker på at du vil slette " + type + "?");
 }
+
+// Testlinje (fjern eller kommenter ut når du har verifisert at filen lastes):
+// alert("funksjoner.js lastet");
+
