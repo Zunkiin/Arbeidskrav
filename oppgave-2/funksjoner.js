133218f674acffb501c80103142feebef48c134f
Zunkiin <zunkinx@gmail.com>
2025-10-18 21:51:20 +0200

    remove

--- oppgave-2/funksjoner.js
+++
@@ -1,8 +0,0 @@
-/* funksjoner.js - brukes til bekreftelse ved sletting */
-
-function bekreft() {
-  return confirm("Er du sikker på at du vil slette denne?");
-}
-
-window.bekreft = bekreft; // gjør funksjonen synlig globalt
-console.log("funksjoner.js lastet");
